5d16e0bc0907499288a51d780301e30eb6294f80
PapaStan <stan.courouble@gmail.com>
2014-07-28 12:13:15 +0200

    add toLower in search

--- app/public/client.js
+++ app/public/client.js
@@ -1,9 +1,9 @@
 function searchInList (event){
-	var searchString = document.getElementById("search").value;
+	var searchString = document.getElementById("search").value.toLowerCase();
 	var lis = document.getElementById("items").getElementsByTagName("li");
     for (i = 0; i < lis.length; i++) {
     	var li = lis[i];
-    	var liText = li.getElementsByTagName("h4")[0].innerHTML;
+    	var liText = li.getElementsByTagName("h4")[0].innerHTML.toLowerCase();
     	alert(liText.indexOf(searchString) > -1);
     	if(liText.indexOf(searchString) > -1) {
 	    	li.removeAttribute("hidden");
